1af528c9c7809db505554411e68f85ef1257384b
LaimaJan <lm.lingyte@gmail.com>
2023-09-12 20:04:41 +0300

    creating shopping crt

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,6 +1,7 @@
 // import { useState } from 'react'
 import Navbar from './components/Navbar/Navbar';
 import PopUpNavbar from './components/PopUpNavbar/PopUpNavbar';
+import ShoppingCart from './components/ShoppingCart/ShoppingCart';
 
 import './App.css';
 
@@ -11,6 +12,7 @@ function App() {
       When width is < 375px the button shows up before the logo, on click the nav bar from left appears. */}
 			<PopUpNavbar />
 			<Navbar />
+			<ShoppingCart />
 		</div>
 	);
 }
